f51619062094a1eaed697747e38fbda9e1498968
Carlos Ibarra <carlosibarra.art@gmail.com>
2019-08-23 14:04:42 -0700

    Created user signup from UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :articles
   before_save { self.email = email.downcase }
+  has_secure_password
 
   validates :username, presence: true,
             uniqueness: { case_sensitive: false },
@@ -10,5 +11,4 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 105},
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
-  has_secure_password
 end
