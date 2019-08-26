74a069b0f42fc95a0ad86d56c9eab269db581ac4
Carlos Ibarra <carlosibarra.art@gmail.com>
2019-08-26 16:50:26 -0700

    Added edit update functionality and partial

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :body, presence: true, length: { minimum: 14, maximum: 300 }
+  validates :body, presence: true, length: { minimum: 100, maximum: 1500 }
   validates :user_id, presence: true
 end
