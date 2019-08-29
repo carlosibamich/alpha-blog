c55fef9c5207c6daaa3c42be28cf363a8ad94ed6
Carlos Ibarra <carlosibarra.art@gmail.com>
2019-08-28 17:06:15 -0700

    Update current user to be article creator and completed authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -17,7 +19,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.last
+    @article.user = current_user
     logger.debug { "New Article: #{@article.attributes.inspect}" }
     logger.debug { "Article should be valid: #{@article.valid?}" }
     if @article.save
@@ -52,5 +54,12 @@ class ArticlesController < ApplicationController
 
     def article_params
       params.require(:article).permit(:title, :body, :user_id)
+    end
+
+    def require_same_user
+      if current_user != @article.user_id
+        flash[:danger] = "You are not authorized to edit this page"
+        redirect_to root_path
+      end
     end
 end
